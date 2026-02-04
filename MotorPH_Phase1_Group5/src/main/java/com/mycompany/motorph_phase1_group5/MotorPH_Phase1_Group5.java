8379263034720d692a20a8d45070b2b28b80875b
Josh <Josh@Project-NiNa>
2026-02-04 14:51:19 +0800

    Commit 1!

---
+++ MotorPH_Phase1_Group5/src/main/java/com/mycompany/motorph_phase1_group5/MotorPH_Phase1_Group5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.motorph_phase1_group5;
+
+/**
+ *
+ * @author Josh
+ */
+public class MotorPH_Phase1_Group5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
